cb32a1f9b1244628c2dccdef5329ff6abe0a400c
Taro Omiya <taro.omiya@omiyagames.com>
2016-04-28 16:00:38 -0400

    Putting together all the scripts and shaders necessary to get the cut-out effect working.

---
+++ Tutorial/Assets/ChangeRenderTexture.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeRenderTexture : MonoBehaviour
+{
+    [SerializeField]
+    Camera renderCamera;
+    [SerializeField]
+    RawImage targetImage;
+
+    // Use this for initialization
+    void Start ()
+    {
+	    if(renderCamera != null)
+        {
+            renderCamera.targetTexture = RenderTextureGenerator.MaskTexture;
+        }
+        if(targetImage != null)
+        {
+            targetImage.texture = RenderTextureGenerator.MaskTexture;
+        }
+	}
+}
---
+++ Tutorial/Assets/RenderTextureGenerator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class RenderTextureGenerator
+{
+    static RenderTexture maskTexture = null;
+
+    public static RenderTexture MaskTexture
+    {
+        get
+        {
+            if(maskTexture == null)
+            {
+                maskTexture = new RenderTexture(Screen.width, Screen.height, 16);
+            }
+            return maskTexture;
+        }
+    }
+}
